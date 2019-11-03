74f40a98a17fc27a8cb6f1fdaaffc836c1d11c18
Shivang Agrawal <shivangagrawal4@gmail.com>
2019-11-03 23:47:20 +0530

    Updates

---
+++ precache-manifest.d1caef10716ff21ba06bf7631fffed5f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f46b1497eb8569f68ab35cb9292d1bb",
+    "url": "/contact-list-react/index.html"
+  },
+  {
+    "revision": "77e30682d9b349ba29f6",
+    "url": "/contact-list-react/static/css/main.db2444a1.chunk.css"
+  },
+  {
+    "revision": "4eb8b6c01bfa72827ea8",
+    "url": "/contact-list-react/static/js/2.59939207.chunk.js"
+  },
+  {
+    "revision": "77e30682d9b349ba29f6",
+    "url": "/contact-list-react/static/js/main.76d3f399.chunk.js"
+  },
+  {
+    "revision": "9c8ecff3dd450428a062",
+    "url": "/contact-list-react/static/js/runtime-main.6b63bfca.js"
+  },
+  {
+    "revision": "4f4f88bc8f4a10a3751ed5ca15ddc809",
+    "url": "/contact-list-react/static/media/add-contact.4f4f88bc.svg"
+  },
+  {
+    "revision": "4abe51fd0358705bc9ace3fe3d81c802",
+    "url": "/contact-list-react/static/media/bin.4abe51fd.svg"
+  },
+  {
+    "revision": "49ba707d0c8bb649a98ab6de1fa50b93",
+    "url": "/contact-list-react/static/media/edit.49ba707d.svg"
+  },
+  {
+    "revision": "a7d974eefbc56673274d9fb1280f7790",
+    "url": "/contact-list-react/static/media/profile.a7d974ee.svg"
+  }
+]);
